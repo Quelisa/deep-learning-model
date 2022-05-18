cb8ef3de0b547496265326d4457a8e522f430ca7
lisaji <lisaji@wezhuiyi.com>
2022-05-18 12:08:59 +0800

    增加MLP模型

--- linear_neural_network/model.py
+++ linear_neural_network/model.py
@@ -1,7 +1,7 @@
 from torch import nn
 
 class Model(nn.Module):
-    """ 模型功能介绍
+    """ linear模型功能介绍
         这是一个只有一层输出维度为1的线性回归模型
         可以用来求解房价预测等线性回归问题
     """
--- multilayer_perceptron/model.py
+++ multilayer_perceptron/model.py
@@ -1,13 +1,15 @@
 from torch import nn
 
 class Model(nn.Module):
-    """ softmax模型功能介绍
+    """ MLP模型功能介绍
         这是一个输出维度大于1，只有一层的的线性回归模型，可以进行分类任务
         该模型可以单独使用，也可以和其他模型叠加使用，用softmax做分类
     """
     def __init__(self, input_dim, label_num):
         super(Model, self).__init__()
-        self.linear = nn.Linear(input_dim, label_num)
+        self.layer1 = nn.Linear(input_dim, hid_dim)
+        self.relu = nn.ReLU()
+        self.layer2 = nn.Linear(hid_dim, label_num)
 
     def forword(self, x):
-        return self.linear(x)
+        return self.layer2(self.relu(self.layer1(x)))
